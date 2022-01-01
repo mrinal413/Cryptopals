3365e3b83b6e80830518d43ed8a984cfaa296789
Mrinalini <73014353+mrinal413@users.noreply.github.com>
2022-01-01 21:34:41 +0530

    Update code.py

--- Challenge Set 1/Implement Repeating key XOR/code.py
+++ Challenge Set 1/Implement Repeating key XOR/code.py
@@ -1,9 +1,16 @@
-plain_text = b"Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
+plaintext = b"Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
 key = b"ICE"
 
-keystream = key*((len(plain_text)//3)+1)
+keystream = key*((len(plaintext)//3)+1)
 
-ciphertext = bytes([x^y for (x,y) in zip(plain_text, keystream)])
+ciphertext = bytes([x^y for (x,y) in zip(plaintext, keystream)])
 
 hexadecimal_string = ciphertext.hex()
-print(hexadecimal_string)
+
+pos = len(plaintext)
+for i in range(len(plaintext)):
+    if i == '\n':
+        pos = i
+        break
+
+print(hexadecimal_string[0:pos],"\n",hexadecimal_string[pos+1:])
