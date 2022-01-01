7badd5777c0da11810ddbea398037578d25d1f4e
Mrinalini <73014353+mrinal413@users.noreply.github.com>
2022-01-01 22:14:45 +0530

    Update fixed_xor.py

--- Challenge Set 1/Fixed XOR/fixed_xor.py
+++ Challenge Set 1/Fixed XOR/fixed_xor.py
@@ -1,7 +1,8 @@
+def xor(a, b):
+    return bytes([x^y for (x,y) in zip(a, b)])
 
-x1 = 0x1c0111001f010100061a024b53535009181c
-x2 = 0x686974207468652062756c6c277320657965
+a = bytes.fromhex('1c0111001f010100061a024b53535009181c')
+b = bytes.fromhex('686974207468652062756c6c277320657965')
 
-xored = x1^x2
-
-print(hex(xored))
+res = xor(a,b)
+print(res)
