9689268b21d4fce1c5ccb7d0feeea67329c771e5
Mrinalini <73014353+mrinal413@users.noreply.github.com>
2022-01-23 16:00:02 +0530

    Update single-byte-xor.py

--- Challenge Set 1/Single-byte XOR cipher/single-byte-xor.py
+++ Challenge Set 1/Single-byte XOR cipher/single-byte-xor.py
@@ -1,20 +1,33 @@
 def xor(a, b):
-    return bytes([x^y for (x,y) in zip(a, b)])
+    return bytes([x^y for (x,y) in zip(a,b)])
 
-def single_byte_xor(ciphertext):
-    best = None
-    for i in range(256): # for every possible ascii chars
-        candidate_key = i.to_bytes(1, byteorder='big')
-        keystream = candidate_key*len(ciphertext)
-        candidate_message = xor(ciphertext, keystream)
-        score = sum([ x in char_list for x in candidate_message])
-        if best == None or score > best['score']:
-            best = {"message": candidate_message, 'score': score, 'key': candidate_key}
-    return best
+def score(ciphertext):
+    s = []
+    for i in range(65,122):
+        keystream = (i.to_bytes(1,byteorder='big'))*len(ciphertext)
+
+        res = xor(ct,keystream)
+        freq = 0
+        for j in res:
+            if chr(j).isalpha():
+                freq += 1
+        
+        a = [freq, chr(i)]
+        s.append(a)
+    return s
+
+ct = bytes.fromhex('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736')
+l = score(ct)
+
+max=0
+key = ' '
+for i in range(57):
+    if l[i][0]>max:
+        max = l[i][0]
+        key = l[i][1]
+print("Message: ", xor(ct,((ord(key)).to_bytes(1,byteorder='big'))*len(ct)), "\nKey: ",key)
 
 
-char_list = list(range(97, 122)) + [32]
-ciphertext = bytes.fromhex('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736')
-result = single_byte_xor(ciphertext)
 
-print('message:', result['message'])
+
+
